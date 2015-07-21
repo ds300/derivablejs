0f03219ffa282a251e0248b30f6cfaa6fbf0fccd
David Sheldrick <ds300@sussex.ac.uk>
2015-07-21 16:04:34 +0100

    Use state-of-the-art equality checks. Yay JS!

--- src/util.js
+++ src/util.js
@@ -19,10 +19,93 @@ export function symbolValues (obj) {
   return Object.getOwnPropertySymbols(obj).map(s => obj[s]);
 }
 
-export function equals (a, b) {
-  return a === b
-         || Object.is && Object.is(a, b)
-         || (a && a.equals && a.equals(b))
+function _type(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1);
+}
+
+function _is(a, b) {
+  // SameValue algorithm
+  if (a === b) { // Steps 1-5, 7-10
+    // Steps 6.b-6.e: +0 != -0
+    return a !== 0 || 1 / a === 1 / b;
+  } else {
+    // Step 6.a: NaN == NaN
+    return a !== a && b !== b;
+  }
+}
+
+function _equals(a, b, stackA, stackB) {
+  var typeA = _type(a);
+  if (typeA !== _type(b)) {
+    return false;
+  }
+
+  if (typeA === 'Boolean' || typeA === 'Number' || typeA === 'String') {
+    return typeof a === 'object' ?
+      typeof b === 'object' && equals(a.valueOf(), b.valueOf()) :
+      false;
+  }
+
+  if (typeA === 'RegExp') {
+    // RegExp equality algorithm: http://stackoverflow.com/a/10776635
+    return (a.source === b.source) &&
+           (a.global === b.global) &&
+           (a.ignoreCase === b.ignoreCase) &&
+           (a.multiline === b.multiline) &&
+           (a.sticky === b.sticky) &&
+           (a.unicode === b.unicode);
+  }
+
+  if (Object(a) === a) {
+    if (typeA === 'Date' && a.getTime() !== b.getTime()) {
+      return false;
+    }
+
+    var keysA = Object.keys(a);
+    if (keysA.length !== Object.keys(b).length) {
+      return false;
+    }
+
+    if (!stackA) {
+      stackA = [];
+      stackB = [];
+    }
+
+    var idx = stackA.length - 1;
+    while (idx >= 0) {
+      if (stackA[idx] === a) {
+        return stackB[idx] === b;
+      }
+      idx -= 1;
+    }
+
+    stackA[stackA.length] = a;
+    stackB[stackB.length] = b;
+    idx = keysA.length - 1;
+    while (idx >= 0) {
+      var key = keysA[idx];
+      if (!Object.hasOwnProperty(key, b) || !equals(b[key], a[key], stackA, stackB)) {
+        return false;
+      }
+      idx -= 1;
+    }
+    stackA.pop();
+    stackB.pop();
+    return true;
+  }
+  return false;
+}
+
+
+export function equals (a, b, stackA, stackB) {
+  if ((Object.is && Object.is(a, b)) || _is(a, b)) {
+    return true;
+  }
+  if (!(a && b)) return false;
+
+  return (typeof a.equals === 'function' && a.equals(b))
+         || (typeof b.equals === 'function' && b.equals(a))
+         || _equals(a, b, stackA, stackB)
          || false;
 }
 
