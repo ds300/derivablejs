4075d3ac1d45c720b35075b8973bd9e0914de074
David Sheldrick <david@futurice.com>
2016-05-01 17:04:04 +0100

    many fewer node versions

--- test/unit/util_test.js
+++ test/unit/util_test.js
@@ -5,9 +5,10 @@ describe('the assign function polyfill', () => {
   it('merges objects together', () => {
     const caps = {a: "A", b: "B"};
     const lower = {a: "a", b: "b", c: "c"};
-    expect(assignPolyfill({}, lower, caps)).to.deep.equal({
+    const init = {};
+    expect(assignPolyfill(init, lower, caps)).to.eql(init);
+    expect(init).to.deep.equal({
       a: "A", b: "B", c: "c"
     });
-
   });
 });
