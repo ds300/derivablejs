ca45bf1ccc0102b7608094e88ac7ad0baade00ec
David Sheldrick <david@futurice.com>
2016-07-24 10:03:36 +0200

    refactor & comment advanced BMI calculator

--- examples/bmi-calculator/react/main.js
+++ examples/bmi-calculator/react/main.js
@@ -3,11 +3,18 @@ import React from 'react';
 import {render} from 'react-dom';
 import {reactive} from 'react-derivable';
 
-
+/**
+ * calculate a body mass index from a weight (in kilograms) and a
+ * height (in centimeters)
+ */
 function bmi (weightKG, heightCM) {
   return Math.round(weightKG / Math.pow(heightCM / 100, 2));
 }
 
+/**
+ * convert a length in centimters to feet and inches components,
+ * rounding to the nearest inch
+ */
 function cm2feetInches (cm) {
   const totalInches = (cm * 0.393701);
   let feet = Math.floor(totalInches / 12);
@@ -19,61 +26,66 @@ function cm2feetInches (cm) {
   return { feet, inches };
 }
 
+/**
+ * convert a length in feet and inches to centimeters, rounding to the
+ * nearest centimeter.
+ */
 function feetInches2cm ({feet, inches}) {
-  return ((feet * 12) + inches) / 0.393701;
+  return Math.round(((feet * 12) + inches) / 0.393701);
 }
 
 
-class SemiControlledInput extends React.Component {
-  componentWillMount () {
-    this.$focused = atom(false);
-  }
-  componentDidMount () {
-    this.props.$Value.react(v => {
-      this.refs.input.value = Math.round(v);
-    }, {
-      when: this.$focused.not()
-    });
-  }
+/**
+ * A controlled input field whose value is backed by an atom
+ */
+const $Input = reactive(class $Input extends React.Component {
   render () {
     const {$Value, ...props} = this.props;
     return (
       <input
         ref="input"
         {...props}
+        value={$Value.get()}
         onChange={e => $Value.set(e.target.value)}
-        onFocus={() => this.$focused.set(true)}
-        onBlur={() => this.$focused.set(false)}
         />
     );
   }
-};
+});
 
+/**
+ * A self-contained component for calculating BMIs in either
+ * imperial or metric measurements
+ */
 const BMICalculator = reactive(class extends React.Component {
   constructor () {
     super();
 
     // define atomic state
-    this.$WeightKG = atom(60);
-    this.$WeightLB = this.$WeightKG.lens({
-      get: kg => kg / 0.453592,
-      set: (_, lb) => lb * 0.453592,
+    this.$WeightLB = atom(150);
+    this.$HeightCM = atom(170);
+
+    // define metric weight lens
+    this.$WeightKG = this.$WeightLB.lens({
+      get: lb => Math.round(lb * 0.453592),
+      set: (_, kg) => Math.round(kg / 0.453592),
     });
-    this.$HeightCM = atom(150);
-    this.$HeightFeetInches = this.$HeightCM.lens({
+
+
+    // define imperial height lenses
+    const $HeightFeetInches = this.$HeightCM.lens({
       get: cm2feetInches,
       set: (_, fi) => feetInches2cm(fi),
     });
-
-    this.$Feet = this.$HeightFeetInches.lens({
+    this.$Feet = $HeightFeetInches.lens({
       get: ({feet}) => feet,
       set: ({inches}, feet) => ({feet: parseInt(feet), inches}),
     });
-    this.$Inches = this.$HeightFeetInches.lens({
+    this.$Inches = $HeightFeetInches.lens({
       get: ({inches}) => inches,
       set: ({feet}, inches) => ({feet, inches: parseInt(inches)})
     });
 
+    // derive BMI and body type classification
     this.$bmi = lift(bmi)(this.$WeightKG, this.$HeightCM);
 
     this.$classification = this.$bmi.derive(bmi => {
@@ -88,30 +100,43 @@ const BMICalculator = reactive(class extends React.Component {
       }
     });
   }
+
   render () {
     return (
       <div>
-        <SemiControlledInput type='number' $Value={this.$WeightLB}/> weight (lb)<br/>
-        <SemiControlledInput type='number' $Value={this.$WeightKG}/> weight (kg)<br/>
-        <SemiControlledInput
-          type='range'
-          $Value={this.$HeightCM}
-          min="100"
-          max="270"/>&nbsp;
-        height: {Math.round(this.$HeightCM.get())}cm <br/>
-        <SemiControlledInput
-          type='range'
-          $Value={this.$Feet}
-          min="3"
-          max="8"/>&nbsp;
-        <SemiControlledInput
-          type='range'
-          $Value={this.$Inches}
-          min="0"
-          max="11"/>&nbsp;
-        height: {this.$Feet.get()}&rsquo;{this.$Inches.get()}&rdquo; <br/>
-        Your BMI: {this.$bmi.get()} <br/>
-        You are: {this.$classification.get()} <br/>
+        <div>
+          <$Input type='number' $Value={this.$WeightLB}/>&nbsp;
+          weight (lb)
+        </div>
+        <div>
+          <$Input type='number' $Value={this.$WeightKG}/>&nbsp;
+          weight (kg)
+        </div>
+        <div>
+          <$Input
+            type='range'
+            $Value={this.$HeightCM}
+            min="100"
+            max="270"/>&nbsp;
+          height: {Math.round(this.$HeightCM.get())}cm
+        </div>
+        <div>
+          <$Input
+            type='range'
+            $Value={this.$Feet}
+            min="3"
+            max="8"/>&nbsp;
+          <$Input
+            type='range'
+            $Value={this.$Inches}
+            min="0"
+            max="11"/>&nbsp;
+          height: {this.$Feet.get()}&rsquo;{this.$Inches.get()}&rdquo;
+        </div>
+        <div>
+          Your BMI is {this.$bmi.get()}
+          , which makes you {this.$classification.get()}.
+        </div>
       </div>
     );
   }
