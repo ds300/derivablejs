66a5c31caac6a2009301277cf556a4b2e0108cf3
David Sheldrick <david@futurice.com>
2017-05-31 11:34:56 +0100

    removed docs folder

--- docs/utils/live-reload-client.js
+++
@@ -1,6 +0,0 @@
-// this gets added to the end of the bundle at dev time
-window.addEventListener('load', function () {
-  new WebSocket("ws://"+window.location.hostname+":__PORT__/").addEventListener('message', function () {
-    window.location.reload();
-  });
-});
--- docs/utils/metalsmith-free-style.js
+++
@@ -1,5 +0,0 @@
-export default function (style, dest) {
-  return function (files) {
-    files[dest] = {contents: new Buffer(style().getStyles()), mode: "0666"};
-  };
-};
