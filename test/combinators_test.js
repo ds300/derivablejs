6117436c45ac2125930b1f52e7ae65b87f54736e
Bogdan Chadkin <trysound@yandex.ru>
2017-11-14 21:58:44 +0300

    Remove map and mMap functions

--- test/combinators_test.js
+++ test/combinators_test.js
@@ -3,79 +3,39 @@
 const derivable = require('../dist/derivable');
 
 test('map derivable value with function', () => {
-  {
-    const a = derivable.atom(1);
-    const q = derivable.atom(10);
-    const b = derivable.map(d => d + q.get(), a);
-    const c = derivable.map(d => d * q.get(), b);
-    expect([b.get(), c.get()]).toEqual([11, 110]);
+  const a = derivable.atom(1);
+  const q = derivable.atom(10);
+  const b = a.map(d => d + q.get());
+  const c = b.map(d => d * q.get());
+  expect([b.get(), c.get()]).toEqual([11, 110]);
 
-    q.set(20);
-    expect([b.get(), c.get()]).toEqual([21, 420]);
+  q.set(20);
+  expect([b.get(), c.get()]).toEqual([21, 420]);
 
-    a.set(null);
-    expect([b.get(), c.get()]).toEqual([20, 400]);
+  a.set(null);
+  expect([b.get(), c.get()]).toEqual([20, 400]);
 
-    expect(() => {
-      derivable.atom().map();
-    }).toThrow();
-  }
-
-  {
-    const a = derivable.atom(1);
-    const q = derivable.atom(10);
-    const b = a.map(d => d + q.get());
-    const c = b.map(d => d * q.get());
-    expect([b.get(), c.get()]).toEqual([11, 110]);
-
-    q.set(20);
-    expect([b.get(), c.get()]).toEqual([21, 420]);
-
-    a.set(null);
-    expect([b.get(), c.get()]).toEqual([20, 400]);
-
-    expect(() => {
-      derivable.map();
-    }).toThrow();
-  }
+  expect(() => {
+    derivable.map();
+  }).toThrow();
 });
 
 test('maybe map derivable (non-null) value with function', () => {
-  {
-    const a = derivable.atom(1);
-    const q = derivable.atom(10);
-    const b = derivable.mMap(d => d + q.get(), a);
-    const c = derivable.mMap(d => d * q.get(), b);
-    expect([b.get(), c.get()]).toEqual([11, 110]);
+  const a = derivable.atom(1);
+  const q = derivable.atom(10);
+  const b = a.mMap(d => d + q.get());
+  const c = b.mMap(d => d * q.get());
+  expect([b.get(), c.get()]).toEqual([11, 110]);
 
-    q.set(20);
-    expect([b.get(), c.get()]).toEqual([21, 420]);
+  q.set(20);
+  expect([b.get(), c.get()]).toEqual([21, 420]);
 
-    a.set(null);
-    expect([b.get(), c.get()]).toEqual([null, null]);
+  a.set(null);
+  expect([b.get(), c.get()]).toEqual([null, null]);
 
-    expect(() => {
-      derivable.atom().map();
-    }).toThrow();
-  }
-
-  {
-    const a = derivable.atom(1);
-    const q = derivable.atom(10);
-    const b = a.mMap(d => d + q.get());
-    const c = b.mMap(d => d * q.get());
-    expect([b.get(), c.get()]).toEqual([11, 110]);
-
-    q.set(20);
-    expect([b.get(), c.get()]).toEqual([21, 420]);
-
-    a.set(null);
-    expect([b.get(), c.get()]).toEqual([null, null]);
-
-    expect(() => {
-      derivable.map();
-    }).toThrow();
-  }
+  expect(() => {
+    derivable.map();
+  }).toThrow();
 });
 
 test('alt', () => {
